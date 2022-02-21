90ccd11f580a72a86d185e7cdd3f596bb55c12f2
NisargShah1410 <nisargshah01410@gmail.com>
2022-02-21 09:14:09 +0000

    first

---
+++ index.js
@@ -0,0 +1,19 @@
+const { exec, execFile } = require('child_process');
+const core = require('@actions/core');
+const github = require('@actions/github');
+var child_process = require('child_process');
+
+try {
+ 
+  exec('go run goexample.go',
+  (error, stdout, stderr) => {
+      console.log(stdout);
+      console.log(stderr);
+      if (error !== null) {
+          console.log(`exec error: ${error}`);
+      }
+  });
+
+} catch (error) {
+  core.setFailed(error.message);
+}
